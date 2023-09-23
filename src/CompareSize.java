fc0ae1fe84f3b05cba61c2521377b48a8ae4f4e0
Hepheir <hepheir@gmail.com>
2023-09-23 17:42:39 +0900

    Implement CompareSize

--- src/CompareSize.java
+++ src/CompareSize.java
@@ -1,6 +1,6 @@
 public class CompareSize implements Comparable {
     @Override
     public int compareTo(Object o1, Object o2) {
-        return 0;
+        return Integer.compare(((FileInfo) o1).getSize(), ((FileInfo) o2).getSize());
     }
 }
