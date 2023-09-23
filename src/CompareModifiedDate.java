07fe02b004e8d8d80beb4ed7e4a33511e268054f
Hepheir <hepheir@gmail.com>
2023-09-23 17:42:31 +0900

    Implement CompareModifiedDate

--- src/CompareModifiedDate.java
+++ src/CompareModifiedDate.java
@@ -1,6 +1,6 @@
 public class CompareModifiedDate implements Comparable {
     @Override
     public int compareTo(Object o1, Object o2) {
-        return 0;
+        return ((FileInfo) o1).getModifiedDate().compareTo(((FileInfo) o2).getModifiedDate());
     }
 }
