6603405138854b8094110db24d97326fd681e7a5
Hepheir <hepheir@gmail.com>
2023-09-23 17:42:16 +0900

    Implement CompareFileName

--- src/CompareFileName.java
+++ src/CompareFileName.java
@@ -1,6 +1,6 @@
 public class CompareFileName implements Comparable {
     @Override
     public int compareTo(Object o1, Object o2) {
-        return 0;
+        return ((FileInfo) o1).getName().compareTo(((FileInfo) o2).getName());
     }
 }
