6895751f4bc99c483335a4fbdc5fe49be93b34f6
Hepheir <hepheir@gmail.com>
2023-09-23 17:42:25 +0900

    Implement CompareFileType

--- src/CompareFileType.java
+++ src/CompareFileType.java
@@ -1,6 +1,6 @@
 public class CompareFileType implements Comparable {
     @Override
     public int compareTo(Object o1, Object o2) {
-        return 0;
+        return ((FileInfo) o1).getType().compareTo(((FileInfo) o2).getType());
     }
 }
